b3b7daef99a59018e22bea15de7faa3c445cbe5c
Ahmed Talaat <ahmed.talaat.ali05@gmail.com>
2026-09-25 09:23:50 +0300

    feat: setup express application

---
+++ app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRouter");
+const taskRoutes = require("./routes/taskRouter");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Connect database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({
+    message: "Todo API is running",
+  });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
